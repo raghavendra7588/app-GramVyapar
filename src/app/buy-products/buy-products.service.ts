ca682fdb33fde40919120e1cd5d6b042e16b34d2
raghavendra7588 <68630618+raghavendra7588@users.noreply.github.com>
2020-10-31 16:11:57 +0530

    changes

--- src/app/buy-products/buy-products.service.ts
+++ src/app/buy-products/buy-products.service.ts
@@ -6,9 +6,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class BuyProductsService {
 
-  //private BASE_URL = 'http://localhost:55547/';
+  private BASE_URL = 'http://localhost:55547/';
 
-  private BASE_URL = 'http://203.112.144.38/uat_InventoryService/';
+  // private BASE_URL = 'http://203.112.144.38/uat_InventoryService/';
   private ADMIN_BASE_URL = 'http://203.112.144.38/uat_AdminApi/api/';
 
   private GET_PRODUCT_LIST = this.ADMIN_BASE_URL + 'Product/GetProductList';
